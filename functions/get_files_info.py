c9b9e4a3087e1f408ae3947cb2143ef24d4ebdb7
NachiketKandari <nachiket.kandari@gmail.com>
2025-11-09 21:16:50 +0530

    removed prints

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -3,7 +3,6 @@ import os
 def get_files_info(working_directory, directory="."):
     abs_working_dir = os.path.abspath(working_directory)
     target_dir = os.path.abspath(os.path.join(working_directory, directory))
-    print(target_dir)
     if not target_dir.startswith(abs_working_dir):
         return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
     if not os.path.isdir(target_dir):
